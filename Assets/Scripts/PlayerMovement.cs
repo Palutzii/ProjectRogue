20daf5ea4365642238b2f42953bd18d4e16f1a22
Pavel Sarmiento <pavelsarmiento@hotmail.com>
2022-06-24 15:39:24 +0200

    ADD: Input keys to movement and adjustment to player in scene

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PlayerMovement : MonoBehaviour
-{
-    
+public class PlayerMovement : MonoBehaviour{
+    public float speed;
+
+    Vector2 direction;
+
+    void Update(){
+        TakeInput();
+        Move();
+    }
+
+    void Move(){
+        transform.Translate(direction * (speed * Time.deltaTime));
+    }
+
+    void TakeInput(){
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W)){
+            direction += Vector2.up;
+        }
+        
+        if (Input.GetKey(KeyCode.A)){
+            direction += Vector2.left;
+        }
+        
+        if (Input.GetKey(KeyCode.S)){
+            direction += Vector2.down;
+        }
+        
+        if (Input.GetKey(KeyCode.D)){
+            direction += Vector2.right;
+        }
+    }
 }
